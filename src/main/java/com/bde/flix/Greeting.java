f5489ce0c7d4e55240f847c1a3f8f9ee464937f4
Bartosz Rybiński <bartoszrybinski@outlook.com>
2023-03-20 18:38:07 +0100

    Initial commit

---
+++ src/main/java/com/bde/flix/Greeting.java
@@ -0,0 +1,4 @@
+package com.bde.flix;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/com/bde/flix/FlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bde.flix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
