37399863b5faba23ca4074891dbccc49fde5e51c
Bartosz Rybiński <bartoszrybinski@outlook.com>
2023-04-02 18:13:18 +0200

    removed getters and setters from Film.java
    Signed-off-by: Bartosz Rybiński <bartoszrybinski@outlook.com>

--- src/main/java/com/bde/flix/model/content/Film.java
+++ src/main/java/com/bde/flix/model/content/Film.java
@@ -13,14 +13,4 @@ public class Film extends Content implements Watchable{
 
     private int duration;
 
-    public int getDuration(){
-        return this.duration;
-    }
-
-    public void setDuration(int dur){
-        if(dur < 0)
-            throw new IllegalArgumentException("Duration must be greater than 0");
-
-        this.duration = dur;
-    }
 }
