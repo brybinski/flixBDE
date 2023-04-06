bdc169e9c30a988ddbd4abb61b1c6879a34d2125
SynProboszcza <miczuja@gmail.com>
2023-04-06 23:10:13 +0200

    POST działa na podstronie /search, testowane za pomocą HackerBar

---
+++ src/main/java/com/bde/flix/Search.java
@@ -0,0 +1,12 @@
+package com.bde.flix;
+
+public record Search(String ID,
+                     String DESCRIPTION,
+                     String DIRECTOR,
+                     String DURATION,
+                     String POSTER,
+                     String RELEASE_DATE,
+                     String TITLE
+) {
+}
+
