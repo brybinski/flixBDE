f578c3bd6212f9ae220ccc402fd2902a67149690
Bartosz Rybiński <bartoszrybinski@outlook.com>
2023-04-14 03:48:05 +0200

    Added testdawid Branch
    Signed-off-by: Bartosz Rybiński <bartoszrybinski@outlook.com>

---
+++ src/main/java/com/bde/flix/controller/UserRecord.java
@@ -0,0 +1,10 @@
+package com.bde.flix.controller;
+
+//import org.springframework.http.HttpStatus;
+
+public record UserRecord(String mail, String passwd) {
+}
+//public record UserRecord(HttpStatus status) {
+//}
+
+//zakomentowane do szybszej implementacji
