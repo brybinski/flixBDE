2258c003d0f4d1407d4a4b488d1efa13ae2b957b
SynProboszcza <miczuja@gmail.com>
2023-05-04 11:57:59 +0200

    Instantiating AdminControllerTest finally works

--- src/test/java/com/bde/flix/controller/AdminControllerTest.java
+++ src/test/java/com/bde/flix/controller/AdminControllerTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.stereotype.Component;
 
 import static com.bde.flix.controller.GenerateRandomValues.NumberLengthOfN;
 import static org.junit.jupiter.api.Assertions.*;
 
-@Component
-@TestConfiguration
+@SpringBootTest
 class AdminControllerTest extends AdminController {
 
     @BeforeEach
@@ -28,30 +28,24 @@ class AdminControllerTest extends AdminController {
     AdminService testAdminService;
     @Test
     void testAdmin() {
-        System.out.println("data");
-        System.out.println("data");
-        double number = Math.random();
-        long numberLong = 0L;
+        long number = 0L;
         try {
-            numberLong = NumberLengthOfN(5);
+            number = NumberLengthOfN(5);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-//        int numberInt = (int)Math.floor(number * 100);
-        String numberStr = Double.toString(number);
+        String numberStr = Long.toString(number);
         String fakeName = "testName" + numberStr;
         String fakeSurname = "testSurname" + numberStr;
         String fakeMail = "testMail" + numberStr;
         String fakePasswd = "testPasswd" + numberStr;
-        String fakeWorkid = "555555" + numberLong;
-        //TODO: repair instantiating testEntity
-        //test fails on this line vv
+        String fakeWorkId = "555" + Long.toString(number);
         Admin testEntity = testAdminService.createadmin(
                 fakeName,
                 fakeSurname,
                 fakeMail,
                 fakePasswd,
-                fakeWorkid
+                fakeWorkId
         );
         assertTrue(true);
     }
