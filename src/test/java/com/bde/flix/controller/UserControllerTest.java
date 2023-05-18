4835f8027f737117c75eddf53573e81d16465d0c
SynProboszcza <miczuja@gmail.com>
2023-05-18 23:13:48 +0200

    Additional testing capabilities to UserControllerTest

--- src/test/java/com/bde/flix/controller/UserControllerTest.java
+++ src/test/java/com/bde/flix/controller/UserControllerTest.java
@@ -1,9 +1,14 @@
 package com.bde.flix.controller;
 
+import com.bde.flix.model.entity.userman.User;
+import com.bde.flix.model.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static com.bde.flix.controller.GenerateRandomValues.NumberLengthOfN;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +24,8 @@ class UserControllerTest extends UserController {
     void tearDown() {
     }
 
+    @Autowired
+    UserRepository testUserRepo;
     @Test
     void testUser() {
         long testingNumber = 0;
@@ -33,7 +40,16 @@ class UserControllerTest extends UserController {
 //        assertEquals(user(testMail, testPasswd).passwd(), testPasswd);
 //        assertNotNull(user(testMail, testPasswd).id());
         UserRecord testingUserInstance = user(testMail, testPasswd);
+
+        List<User> testUserRepoInstance = testUserRepo.findAll();
+        int testIndex = testUserRepoInstance.size()-1; // Newest possible index
+
+        // checking if returned data is correct
         assertEquals(testMail, testingUserInstance.mail());
         assertEquals(testPasswd, testingUserInstance.passwd());
+
+        //pulling the same data from database
+        assertEquals(testMail, testUserRepoInstance.get(testIndex).getEmail());
+        assertEquals(testPasswd, testUserRepoInstance.get(testIndex).getHash());
     }
 }
