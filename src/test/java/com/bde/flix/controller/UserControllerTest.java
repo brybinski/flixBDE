42080334b6433b3dbfb85c97cd003247a0b40d43
SynProboszcza <miczuja@gmail.com>
2023-05-18 23:05:07 +0200

    UserControllerTest is working

--- src/test/java/com/bde/flix/controller/UserControllerTest.java
+++ src/test/java/com/bde/flix/controller/UserControllerTest.java
@@ -3,10 +3,12 @@ package com.bde.flix.controller;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.bde.flix.controller.GenerateRandomValues.NumberLengthOfN;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class UserControllerTest extends UserController {
 
     @BeforeEach
@@ -27,8 +29,11 @@ class UserControllerTest extends UserController {
         }
         String testMail = "testing" + testingNumber + "@testbde.test";
         String testPasswd = "testing" + testingNumber + "password";
-        assertEquals(user(testMail, testPasswd).mail(), testMail);
-        assertEquals(user(testMail, testPasswd).passwd(), testPasswd);
-        assertNotNull(user(testMail, testPasswd).id());
+//        assertEquals(user(testMail, testPasswd).mail(), testMail);
+//        assertEquals(user(testMail, testPasswd).passwd(), testPasswd);
+//        assertNotNull(user(testMail, testPasswd).id());
+        UserRecord testingUserInstance = user(testMail, testPasswd);
+        assertEquals(testMail, testingUserInstance.mail());
+        assertEquals(testPasswd, testingUserInstance.passwd());
     }
 }
